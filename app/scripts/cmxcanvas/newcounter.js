44b4bbcb771d7d0e651c94175261c576ecd5787a
roshow <rolando@roshow.net>
2014-04-17 23:38:50 -0400

    new counter is now, actually, a way of extending the collection of story panels

--- app/scripts/cmxcanvas/newcounter.js
+++ app/scripts/cmxcanvas/newcounter.js
@@ -1,47 +1,79 @@
-'use strict';
-var loc = [0,0],
-    cmxjson,
-    request = new XMLHttpRequest();
-request.open('GET', 'json/sov01Model.json', true);
-request.onload = function(){
-    // cmxcanvas.load(JSON.parse(this.response), 'thisCanvas');
-};
-request.send();
+'use strict'
 
-function next(){
-    if (cmxjson[loc[0]].popups && loc[1] + 1 < cmxjson[loc[0]].popups.length){
-        loc[1]++;
-        console.log('panel: '+cmxjson[loc[0]].src+', popup: ' + cmxjson[loc[0]].popups[loc[1]].src);
-    }
-    else if (loc[0] + 1 < cmxjson.length){
-        loc[0]++;
-        loc[1] = -1;
-        console.log('panel: '+cmxjson[loc[0]].src+', popup: -1');
-    }
-    else {
-        console.log('NO MAS!');
-    }
-    return loc;
-};
+function StoryPanels(cmxjson){
+  
+    var loc = [0,-1];
 
-function prev(){
-    if (loc[0] - 1 >= 0){
-        loc[0]--;
-        loc[1] = -1;
-        console.log('panel: '+cmxjson[loc[0]].src+', popup: -1');
+    function getView(){
+        var view = cmxjson[loc[0]];
+        view.type = 'panel';
+        if (loc[1] !== -1){
+            view = view.popups[loc[1]];
+            view.popup = loc[1];
+            view.type = 'popup';
+        }
+        view.panel = loc[0];
+        return view;
     }
-    else if (loc[0] === 0 && loc[1] !== -1){
-        loc[1] = -1;
-        console.log('panel: '+cmxjson[loc[0]].src+', popup: -1');
+
+    function next(){
+        if (cmxjson[loc[0]].popups && loc[1] + 1 < cmxjson[loc[0]].popups.length){
+            loc[1]++;
+            return getView();
+        }
+        else if (loc[0] + 1 < cmxjson.length){
+            loc[0]++;
+            loc[1] = -1;
+            return getView();
+        }
+        else {
+            console.log('NO MAS!');
+        }
+        return loc;
     }
-    else {
-        console.log('MUY POCO!');
+
+    function prev(){
+        if (loc[0] - 1 >= 0){
+            loc[0]--;
+            loc[1] = -1;
+            return getView();
+        }
+        else if (loc[0] === 0 && loc[1] !== -1){
+            loc[1] = -1;
+            return getView();
+        }
+        else {
+            console.log('MUY POCO!');
+        }
+        return loc;
     }
-    return loc;
+
+    function goTo(panel, popup){
+        loc[0] = panel || 0;
+        loc[1] = popup || -1;
+        return getView();
+    }
+  
+    cmxjson.next = next;
+    cmxjson.prev = prev;
+    cmxjson.goTo = goTo;
+    cmxjson.getView = getView;
+    return cmxjson;
 }
 
-function goTo(panel, popup){
-    loc[0] = panel || 0;
-    loc[1] = popup || -1;
-    return loc;
-}
+// var storypanels;
+
+// $.getJSON('http://cmxcanvasapi.herokuapp.com/cmx/sov01')
+    .done(function(data){
+      
+        storypanels = StoryPanels(data.data[0].cmxJSON);
+      
+        console.log(storypanels.getView());
+        $('#next').on('click', function(){
+            console.log(storypanels.next());
+        });
+        $('#prev').on('click', function(){
+            console.log(storypanels.prev());
+        });
+      
+    });
