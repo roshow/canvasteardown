296c98bb3095701a18f2d68939a49d0a6c17b1cf
roshow <rolando@roshow.net>
2014-06-23 23:39:39 -0400

    put in more complete shims for requestAnimation and performance.now

--- app/scripts/cmxcanvas/roquestAnimation.js
+++ app/scripts/cmxcanvas/roquestAnimation.js
@@ -4,33 +4,43 @@
 'use strict';
 
 var roquestAnim = (function(){
-    var reqAF = requestAnimationFrame || webkitRequestAnimationFrame;
+    // var requestAnimFrame = requestAnimationFrame || webkitRequestAnimationFrame;
 
-    var now = (function() {
-     
-      // Returns the number of milliseconds elapsed since either the browser navigationStart event or 
-      // the UNIX epoch, depending on availability.
-      // Where the browser supports 'performance' we use that as it is more accurate (microsoeconds
-      // will be returned in the fractional part) and more reliable as it does not rely on the system time. 
-      // Where 'performance' is not available, we will fall back to Date().getTime().
-     
-      // jsFiddle: http://jsfiddle.net/davidwaterston/xCXvJ
-     
-     
-        var performance = window.performance || {};
-        
-      performance.now = (function() {
+    
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       ||
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+    var performance = window.performance || {};
+    performance.now = (function() {
         return performance.now    ||
         performance.webkitNow     ||
         performance.msNow         ||
         performance.oNow          ||
         performance.mozNow        ||
         function() { return new Date().getTime(); };
-      })();
-              
-      return performance.now();     
+    })();
+
+    // var now = (function() {
      
-    });
+    //   // Returns the number of milliseconds elapsed since either the browser navigationStart event or 
+    //   // the UNIX epoch, depending on availability.
+    //   // Where the browser supports 'performance' we use that as it is more accurate (microsoeconds
+    //   // will be returned in the fractional part) and more reliable as it does not rely on the system time. 
+    //   // Where 'performance' is not available, we will fall back to Date().getTime().
+     
+    //   // jsFiddle: http://jsfiddle.net/davidwaterston/xCXvJ
+     
+     
+        
+              
+    //     return performance.now();     
+     
+    // });
 
     function roquest(animFunc, lenAnim){
         var deferred = new Q.defer(),
@@ -42,24 +52,24 @@ var roquestAnim = (function(){
             if (animReturnVal === false) {
                 deferred.resolve();
             } else {
-                rAF = reqAF(animLoop);
+                rAF = requestAnimFrame(animLoop);
             }
         }
 
         function timeBasedLoop(){
-            var timePassed = now() - animStartTime;
+            var timePassed = performance.now() - animStartTime;
             if (timePassed >= lenAnim) {
                 animFunc(lenAnim);
                 deferred.resolve();
             } else {
                 animFunc(timePassed);
-                rAF = reqAF(timeBasedLoop);
+                rAF = requestAnimFrame(timeBasedLoop);
             }
 
         }
 
-        animStartTime = now();
-        rAF = lenAnim ? reqAF(timeBasedLoop) : reqAF(animLoop);
+        animStartTime = performance.now();
+        rAF = lenAnim ? requestAnimFrame(timeBasedLoop) : requestAnimFrame(animLoop);
 
         return deferred.promise;
     }
